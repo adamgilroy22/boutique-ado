39c8085d3035fe028a943a6e7db9855f84c0037f
Adam <adamgilroy22@gmail.com>
2023-04-18 20:20:58 +0100

    Set up for local development

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')  # noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-ado-adam-gilroy.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-adam-gilroy.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
